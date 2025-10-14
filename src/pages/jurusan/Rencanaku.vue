1a25135094e4f9a292f2c5c04675cf208145af01
Saladin <saladinoctianobonanza@gmail.com>
2025-10-14 11:20:06 +0700

    add button rencanaku

--- src/pages/jurusan/Rencanaku.vue
+++ src/pages/jurusan/Rencanaku.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="">
+  <section class="mb-5 md:mb-0 md:mt-20">
     <!-- TKJ -->
     <div class="flex flex-col-reverse md:flex-row items-center justify-between">
       <!-- Teks -->
@@ -7,21 +7,10 @@
         <h2 class="text-4xl font-extrabold text-gray-900">
           Teknik Komputer Jaringan
           <span
-            class="bg-red-500 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 1v22m6-10a6 6 0 01-12 0"
-              />
+            class="bg-merah text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 1v22m6-10a6 6 0 01-12 0" />
             </svg>
             TKJ
           </span>
@@ -29,17 +18,16 @@
         <p class="text-gray-900">
           TKJ adalah jurusan yang mendalami cara-cara merakit komputer dan instalasi jaringan komputer.
         </p>
-        <button class="bg-red-500 text-white rounded p-3">
-          <h1>Lebih Detail</h1>
-        </button>
+        <RouterLink to="/jurusan/tkj"
+          class="bg-red-600 text-white flex items-center justify-center gap-2 p-4 shadow-md hover:shadow-xl hover:scale-105 active:bg-gray-100 active:shadow-sm transition w-60">
+          Lihat Jurusan TKJ
+        </RouterLink>
       </div>
 
       <!-- Gambar -->
       <div class="w-full relative mt-10 md:my-0">
-        <div
-          class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-1"
-          :style="{ backgroundImage: `url(${tkjImg})` }"
-        ></div>
+        <div class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-1"
+          :style="{ backgroundImage: `url(${tkjImg})` }"></div>
       </div>
     </div>
 
@@ -49,21 +37,10 @@
         <h2 class="text-4xl font-extrabold text-gray-900">
           Teknik Jaringan Akses
           <span
-            class="bg-yellow-400 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 1v22m6-10a6 6 0 01-12 0"
-              />
+            class="bg-yellow-400 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 1v22m6-10a6 6 0 01-12 0" />
             </svg>
             TJA
           </span>
@@ -72,36 +49,27 @@
           TJA adalah jurusan yang berkompetensi di bidang jaringan, baik itu jaringan akses kabel
           (tembaga, coaxial dan fiber optic), maupun nirkabel.
         </p>
+        <RouterLink to="/jurusan/tja"
+          class="bg-merah text-white flex items-center justify-center gap-2 p-4 shadow-md hover:shadow-xl hover:scale-105 active:bg-gray-100 active:shadow-sm transition w-60">
+          Lihat Jurusan TJA
+        </RouterLink>
       </div>
       <div class="w-full relative mt-10 md:my-0">
-        <div
-          class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-2"
-          :style="{ backgroundImage: `url(${tjaImg})` }"
-        ></div>
+        <div class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-2"
+          :style="{ backgroundImage: `url(${tjaImg})` }"></div>
       </div>
     </div>
-    
+
     <!-- RPL -->
     <div class="flex flex-col-reverse md:flex-row items-center justify-between">
       <div class="w-full space-y-4 mx-5 p-5 md:p-0">
         <h2 class="text-4xl font-extrabold text-gray-900">
           Rekayasa Perangkat Lunak
           <span
-            class="bg-green-500 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 1v22m6-10a6 6 0 01-12 0"
-              />
+            class="bg-green-500 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 1v22m6-10a6 6 0 01-12 0" />
             </svg>
             RPL
           </span>
@@ -109,12 +77,15 @@
         <p class="text-gray-900">
           RPL adalah jurusan yang mendalami cara-cara pengembangan perangkat lunak (software).
         </p>
+        <RouterLink to="/jurusan/rpl"
+          class="bg-merah text-white flex items-center justify-center gap-2 p-4 shadow-md hover:shadow-xl hover:scale-105 active:bg-gray-100 active:shadow-sm transition w-60">
+          Cek Lebih Lanjut
+        </RouterLink>
+
       </div>
       <div class="w-full relative mt-10 md:my-0">
-        <div
-          class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-1"
-          :style="{ backgroundImage: `url(${rplImg}` }"
-        ></div>
+        <div class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-1"
+          :style="{ backgroundImage: `url(${rplImg}` }"></div>
       </div>
     </div>
 
@@ -124,34 +95,26 @@
         <h2 class="text-4xl font-extrabold text-gray-900">
           Pengembangan Gim
           <span
-            class="bg-blue-500 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="w-4 h-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 1v22m6-10a6 6 0 01-12 0"
-              />
+            class="bg-blue-500 text-white text-sm font-semibold px-2 py-1 rounded-md ml-2 inline-flex items-center gap-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 1v22m6-10a6 6 0 01-12 0" />
             </svg>
             PG
           </span>
         </h2>
         <p class="text-gray-900">
-          PG adalah jurusan yang berfokus pada pembuatan dan pengembangan permainan digital berbasis komputer dan perangkat mobile.
+          PG adalah jurusan yang berfokus pada pembuatan dan pengembangan permainan digital berbasis komputer dan
+          perangkat mobile.
         </p>
+        <RouterLink to="/jurusan/pg"
+          class="bg-merah text-white flex items-center justify-center gap-2 p-4 shadow-md hover:shadow-xl hover:scale-105 active:bg-gray-100 active:shadow-sm transition w-60">
+          Cek Lebih Lanjut
+        </RouterLink>
       </div>
       <div class="w-full relative mt-10 md:my-0">
-        <div
-          class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-2"
-          :style="{ backgroundImage: `url(${pgImg})` }"
-        ></div>
+        <div class="w-full h-60 md:h-[300px] bg-cover bg-center transition-all duration-500 clip-diagonal-2"
+          :style="{ backgroundImage: `url(${pgImg})` }"></div>
       </div>
     </div>
   </section>
@@ -174,5 +137,11 @@ import pgImg from '@/assets/images/pg_images.jpg'
   .clip-diagonal-2 {
     clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 20% 100%);
   }
+}
+</style>
+
+<style scoped>
+.bg-merah {
+  @apply bg-red-600;
 }
 </style>
