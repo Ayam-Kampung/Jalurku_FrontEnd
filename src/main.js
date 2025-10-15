abfeb8da9c1d74eb461a39e44d9fb66c440e4091
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2025-10-15 23:09:43 +0700

    feat:css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/main.css'
+import './style.css'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
 
