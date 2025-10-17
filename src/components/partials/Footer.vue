3899d5913577b7f3539f42461c32f4476dd9a4f0
ALFADHANI284 <alfadhanifahrezan@gmail.com>
2025-10-17 13:50:07 +0700

    feat:Footer

--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -46,7 +46,7 @@
             <path
               d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480v58q0 59-40.5 100.5T740-280q-35 0-66-15t-52-43q-29 29-65.5 43.5T480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480v58q0 26 17 44t43 18q26 0 43-18t17-44v-58q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93h200v80H480Zm0-280q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Z" />
           </svg>
-          email@smktelkom-pwt.sch.id
+          smktelkom-pwt.sch.id
         </a>
 
         <a href="#" class="flex items-center gap-2 font-mono hover:underline">
@@ -72,8 +72,24 @@
             Beranda
           </RouterLink>
         </li>
+         <li>
+          <RouterLink to="/playground" class="flex items-center gap-2 hover:text-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
+              <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z" />
+            </svg>
+            Playground
+          </RouterLink>
+        </li>
         <li>
           <RouterLink to="/rencanaku" class="flex items-center gap-2 hover:text-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
+              <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z" />
+            </svg>
+            Rencanaku
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/jalurku" class="flex items-center gap-2 hover:text-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF">
               <path d="m256-240-56-56 384-384H240v-80h480v480h-80v-344L256-240Z" />
             </svg>
@@ -101,19 +117,13 @@
               d="M16,2.345c7.735,0,14,6.265,14,14-.002,6.015-3.839,11.359-9.537,13.282-.7,.14-.963-.298-.963-.665,0-.473,.018-1.978,.018-3.85,0-1.312-.437-2.152-.945-2.59,3.115-.35,6.388-1.54,6.388-6.912,0-1.54-.543-2.783-1.435-3.762,.14-.35,.63-1.785-.14-3.71,0,0-1.173-.385-3.85,1.435-1.12-.315-2.31-.472-3.5-.472s-2.38,.157-3.5,.472c-2.677-1.802-3.85-1.435-3.85-1.435-.77,1.925-.28,3.36-.14,3.71-.892,.98-1.435,2.24-1.435,3.762,0,5.355,3.255,6.563,6.37,6.913-.403,.35-.77,.963-.893,1.872-.805,.368-2.818,.963-4.077-1.155-.263-.42-1.05-1.452-2.152-1.435-1.173,.018-.472,.665,.017,.927,.595,.332,1.277,1.575,1.435,1.978,.28,.787,1.19,2.293,4.707,1.645,0,1.173,.018,2.275,.018,2.607,0,.368-.263,.787-.963,.665-5.719-1.904-9.576-7.255-9.573-13.283,0-7.735,6.265-14,14-14Z" />
           </svg>
         </a>
-        <a href="">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#FFFFFF" class="w-6 h-6">
-            <path
-              d="M357.2 48L427.8 48 273.6 224.2 455 464 313 464 201.7 318.6 74.5 464 3.8 464 168.7 275.5-5.2 48 140.4 48 240.9 180.9 357.2 48zM332.4 421.8l39.1 0-252.4-333.8-42 0 255.3 333.8z" />
-          </svg>
-        </a>
-        <a href="">
+        <a href="https://www.youtube.com/@stematelpwt">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="#FFFFFF" class="w-6 h-6">
             <path
               d="M549.7 124.1C543.5 100.4 524.9 81.8 501.4 75.5 458.9 64 288.1 64 288.1 64S117.3 64 74.7 75.5C51.2 81.8 32.7 100.4 26.4 124.1 15 167 15 256.4 15 256.4s0 89.4 11.4 132.3c6.3 23.6 24.8 41.5 48.3 47.8 42.6 11.5 213.4 11.5 213.4 11.5s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zM232.2 337.6l0-162.4 142.7 81.2-142.7 81.2z" />
           </svg>
         </a>
-        <a href="">
+        <a href="https://www.instagram.com/stematelpwt/">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="#FFFFFF" class="w-6 h-6">
             <path
               d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
@@ -128,21 +138,12 @@
         <img class="h-5 w-5" :src="Favicon" alt="Favicon" />
         <p class="font-mono text-sm">(C) 2025 Ayam Kampung. All rights reserved.</p>
       </div>
-      <!-- <div class="flex items-center gap-2">
-        <img class="h-20 w-20" :src="JIHCLogo" alt="Favicon" />
-        <img class="h-20 w-20" :src="KOMDIGILogo" alt="Favicon" />
-        <img class="h-20 w-20" :src="MASPIONLogo" alt="Favicon" />
-        <img class="h-20 w-20" :src="I000Logo" alt="Favicon" />
-      </div> -->
     </div>
   </footer>
 </template>
 
 <script setup>
 import Favicon from '@/assets/images/favicon.webp'
-import JIHCLogo from '@/assets/images/Logo-JHIC.webp'
-import KOMDIGILogo from '@/assets/images/Logo-KOMDIGI.webp'
-import MASPIONLogo from '@/assets/images/Logo-MASPION.webp'
 
 </script>
 
